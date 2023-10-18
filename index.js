8da9a82b6ff0bc3fd3ba624a7084ecb96487b182
mehedihasanepu <mehedihasanepu123@gmail.com>
2023-10-18 17:36:01 +0600

    create server side and added index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware 
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/',(req,res)=>{
+    res.send('TrendSway server is running')
+})
+
+app.listen(port,()=>{
+    console.log(`TrendSway server is running on port: ${port}`);
+})
+
+
+
+
+
+
+
